2a36c62d9928bf30d233ad0a1f50937f3a536efe
Izza <icg417@gmail.com>
2016-10-28 11:20:28 -0400

    building up Jayus controllers/models/tables - basics

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,30 @@
+class CommentsController < ApplicationController
+	before_action :find_post
+
+	
+	def create
+		@comment = @user.comments.create(params[:comment].permit(:content))
+		@comment.user.id = current_user.id
+		@comment.save
+
+		if @comment.save
+			redirect_to post_path(@post)
+		else
+			render 'new'
+		end
+	end
+
+
+
+
+
+
+
+	private
+
+	def find_post
+		@post = Post.find(params[:post_id])
+	end
+
+
+end
